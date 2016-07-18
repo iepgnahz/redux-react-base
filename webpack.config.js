ecb177f59c11d997f10fdcaf6e440507ca8daece
linwenjun <linwenjun@163.com>
2016-07-18 22:12:09 +0800

    add source map support

--- webpack.config.js
+++ webpack.config.js
@@ -31,12 +31,8 @@ module.exports = {
           }
       ]
   },
+  devtool: "#source-map"
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin("vendors", "vendors.js"),
-    // new webpack.optimize.UglifyJsPlugin({
-    //     compress: {
-    //         warnings: false
-    //     }
-    // })
+    new webpack.optimize.CommonsChunkPlugin("vendors", "vendors.js")
   ]
 };
